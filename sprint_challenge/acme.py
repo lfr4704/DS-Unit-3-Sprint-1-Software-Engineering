1a9d95efacf5e875bbd36c664d48ec0ab625585e
Luis Robles <lfr4704@gmail.com>
2020-05-17 19:27:29 -0700

    added stealability and explode methods to acme

--- sprint_challenge/acme.py
+++ sprint_challenge/acme.py
@@ -23,6 +23,36 @@ class Product():
         self.flammability = flammability
         self.identifier = identifier
 
+    def stealability(self):
+        """
+        calculates the price divided by the weight, and then returns a message: if the ratio
+        is less than 0.5 return "Not so stealable...", if it is greater or equal to 0.5 but
+        less than 1.0 return "Kinda stealable.", and otherwise return "Very stealable!"
+        """
+        price_weight_ratio = self.price/self.weight
+        if price_weight_ratio < 0.5:
+            return "Not so stealable ..."
+        elif price_weight_ratio >= 0.5 and price_weight_ratio < 1:
+            return "Kinda stealable"
+        else:
+            return "Very stealable"
+
+    def explode(self):
+        """
+        Calculates the flammability times the weight, and then returns a message: if the product
+        is less than 10 return "...fizzle.", if it is greater or equal to 10 but less than 50
+        return "...boom!", and otherwise return "...BABOOM!!"
+        """
+
+        explosion_power = self.flammability * self.weight
+        if explosion_power < 10:
+            return "fizzle"
+        elif explosion_power >= 10 and explosion_power < 50:
+            return "...bom!"
+        else:
+            return "...BABOOM!!"
+
+
 
 
 
